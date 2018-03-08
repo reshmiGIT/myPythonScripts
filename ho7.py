96017b1b9f1ff1a12cfbef366a8dc4d8d9629121
reshmiGIT <37097464+reshmiGIT@users.noreply.github.com>
2018-03-08 10:10:07 +0530

    Add files via upload

---
+++ ho7.py
@@ -0,0 +1,8 @@
+n=int(input())
+if 2 <= n <= 10:
+    arr=map(int,input().split())
+    #print(sorted(set(arr))[-2])
+    for i in range(1,n+1):
+        if -100 <= arr[i] <= 100:
+            
+     
